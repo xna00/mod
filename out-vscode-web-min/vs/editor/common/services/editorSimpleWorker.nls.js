e9b350c1248e239c7b44598c65669b85bda0db47
xna00 <61122789+xna00@users.noreply.github.com>
2024-05-03 12:39:03 +0000

    Deploying to gh-pages from @ xna00/mod@6dba07f8b3030f06170c345f677b3baa75dfdb12 🚀

---
+++ out-vscode-web-min/vs/editor/common/services/editorSimpleWorker.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/define("vs/editor/common/services/editorSimpleWorker.nls",{"vs/base/common/platform":["_"],"vs/editor/common/languages":["array","boolean","class","constant","constructor","enumeration","enumeration member","event","field","file","function","interface","key","method","module","namespace","null","number","object","operator","package","property","string","struct","type parameter","variable","{0} ({1})"]});
+
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/e170252f762678dec6ca2cc69aba1570769a5d39/core/vs/editor/common/services/editorSimpleWorker.nls.js.map
