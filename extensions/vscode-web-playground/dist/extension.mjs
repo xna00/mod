b785d9d7ae85111faa8470a0a06989a6ee93351c
xna00 <61122789+xna00@users.noreply.github.com>
2024-05-13 14:19:56 +0000

    Deploying to gh-pages from @ xna00/mod@dd315db1c62ab507d0490a6cb695a8250102177a 🚀

--- extensions/vscode-web-playground/dist/extension.mjs
+++ extensions/vscode-web-playground/dist/extension.mjs
@@ -16169,12 +16169,21 @@ let a = 1
 let id = fun x -> x
 let ( ++ ) = ( + )
 let b = 2 ++ 2
+
+let o1 = {a=1}
+let o2 = {b = 2 | o1}
+let f o = o.a + o.b
+
+let m x = match x with
+| \`Int y -> y
+| \`Float y -> 0
+
+
 let e = <div></div>
 let button ~className ?size = <div className={className}> </div>
 
 let e2 = <button className={1}></button>
 
-let 
 module M = struct
 	let make = button ~size:3
 end
