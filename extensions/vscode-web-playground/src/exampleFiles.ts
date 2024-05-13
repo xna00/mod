61c6ebffc767b6f9ae37802eaf9a24678e2b2ff8
xna00 <61122789+xna00@users.noreply.github.com>
2024-05-13 09:50:23 +0000

    Deploying to gh-pages from @ xna00/mod@dccd58a205f3d2be080efd8c731fa74a1f1c6cd6 🚀

--- extensions/vscode-web-playground/src/exampleFiles.ts
+++ extensions/vscode-web-playground/src/exampleFiles.ts
@@ -3,8 +3,18 @@ export const test_mod =
 (* Hover to inspect the type of a expression *)
 
 let a = 1
+let id = fun x -> x
+let ( ++ ) = ( + )
+let b = 2 ++ 2
+let e = <div></div>
+let button ~className ?size = <div className={className}> </div>
+
+let e2 = <button className={1}></button>
+
+let 
 module M = struct
-	let id = fun x -> x
-	let ( ++ ) = ( + )
+	let make = button ~size:3
 end
+
+let e4 = <M className={0}></M>
 `.trim()
